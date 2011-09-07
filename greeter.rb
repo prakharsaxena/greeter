3babbbc44e2e02fa874ae0d05c0642c3f90bde77
Prakhar Saxena <prakharsaxena@gmail.com>
2011-09-07 14:22:36 -0400

    Added stuff for greeter

--- greeter.rb
+++ greeter.rb
@@ -1,11 +1,39 @@
 #!/usr/bin/env ruby
 # This script will greet people
 
-class Greeter
+module GreetingAndFarewell
+  
+  def name
+    @name ||= "Some other person"
+  end
+  
+  def name= new_name
+    @name = new_name
+  end
+  
 	def hello name
-		puts "Hello #{name}!"
+		p "Hello #{name}!"
+	end
+	
+	def farewell
+	  p "Farewell, #{name}!"
 	end
 end
 
-myname = Greeter.new
-myname.hello ARGV.first
+
+class Greeter
+  include GreetingAndFarewell
+  def initialize new_name = nil
+    @name = new_name
+  end
+end
+
+class Customer
+  include GreetingAndFarewell
+  def name
+    @name ||= "Generated Name in Customer"
+  end
+end
+
+#myname = Greeter.new
+#myname.hello ARGV.first
